2ed4bc6ea4de74182ed5300fb9008d99286bab56
RaymondKlass <raymond@klassphoto.com>
2014-11-02 13:41:38 -0500

    added middleware example hook

--- express_service/git-service/app.js
+++ express_service/git-service/app.js
@@ -26,7 +26,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(function(req, res, next) {
-    console.log('something here');
+    console.log('something is happening here...');
     next();
 });
 
